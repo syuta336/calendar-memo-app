815d00126186a09b04875e2e995cfe7a44ee7abc
yuta <y.suzuki336@gmail.com>
2025-05-06 16:49:49 +0900

    deployファイルのサーバーnpm run 設定編集

---
+++ backend/src/models/Event.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+const eventSchema = new Schema({
+    // id: { type: String, required: true, default: () => new mongoose.Types.ObjectId().toString() },
+    event: { type: String, required: true },
+    date: { type: Date, required: true },
+}, { timestamps: true });
+eventSchema.set('toJSON', {
+    virtuals: true,
+    transform: (_, ret) => {
+        const { _id, __v, ...rest } = ret;
+        return {
+            id: _id.toString(), // `id` を最初に配置
+            ...rest, // 残りのプロパティを展開
+        };
+    },
+});
+const Event = model('Event', eventSchema);
+export default Event;
---
+++ backend/src/routes/index.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import eventRoutes from './eventRoutes';
+const router = express.Router();
+router.use('/events', eventRoutes);
+export default router;
